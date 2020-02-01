97855f70ae724b45488e86302bcd6fd2db6e83f4
naunow <naunow00@gmail.com>
2020-02-01 16:58:45 +0900

    『テスト駆動開発』 Written by Kent Beck
    Copy Coding P.285~ フィボナッチ

---
+++ FibonacciTests/ProgramTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Fibonacci;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fibonacci.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        public int Fib(int n)
+        {
+            return 0;
+        }
+
+        [TestMethod()]
+        public void TestFibonacci()
+        {
+            Assert.AreEqual(0, Fib(0));
+        }
+    }
+}
